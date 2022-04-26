36a9e2e7b976fc51e57045a71d1c169d9c50496a
Seb Naylor <sebnaylor@Sebs-MacBook-Air.local>
2022-04-26 18:02:23 +0100

    Finished pages and made simple styling changes - added a try at skipping authentification for static pages

--- config/routes.rb
+++ config/routes.rb
@@ -6,5 +6,10 @@ Rails.application.routes.draw do
   resources :meetings
   devise_for :users
   root to: 'pages#home'
+  get 'public_toilets_survey', to: "pages#public_toilets_survey"
+  get 'facts_figures', to: "pages#facts_figures"
+  get 'gta_report', to: "pages#gta_report"
+  get 'gta_report_response', to: "pages#gta_report_response"
+  get 'wra_constitution', to: "pages#wra_constitution"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
