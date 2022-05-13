9dfea47cc03f04e192780035a61e32081ed9fa8f
Seb Naylor <sebnaylor@Sebs-MacBook-Air.local>
2022-05-13 15:52:21 +0100

    added comments

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,3 +1,16 @@
 class CommentsController < ApplicationController
-    #unused for MVP
+
+  def create
+    @post = Post.find(params[:post_id])
+    @comment = Comment.new(post: @post, user_name: current_user.name, text: params[:comment][:text])
+    @comment.save
+    redirect_to post_path(@post)
+  end
+
+  private
+  
+  def comment_params
+    params.require(:comment).permit(:text)
+  end
+
 end
